3964bc669d19daa233a30cb76f0fb109b99fcbe7
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-22 12:28:37 +0530

    backend live url updated

--- Frontend/src/assets/Components/api/axios.js
+++ Frontend/src/assets/Components/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL:"http://localhost:9937/",
+    baseURL:"https://quickdrop-backend-zn0l.onrender.com/",
+    // baseURL:"http://localhost:9937/",
     withCredentials:true
 })
 
